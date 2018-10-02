5b846583f8b268360791aa16b1ab1862a6920f7a
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:24:12 +0300

    added repository for Platform

--- src/main/java/com/bombaysour/bombaysour/repository/PlatformRepository.java
+++ src/main/java/com/bombaysour/bombaysour/repository/PlatformRepository.java
@@ -1,4 +1,16 @@
 package com.bombaysour.bombaysour.repository;
 
-public interface PlatformRepository {
+import com.bombaysour.bombaysour.model.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlatformRepository extends JpaRepository<Platform,Long> {
+
+    List<Platform> findAllByAvailable(Boolean available);
+
+    Platform findByAvailableAndId(Boolean available, Long id);
+
 }
