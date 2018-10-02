e3ba0baa01a1ec2e899f64e56f9500c9ef508e07
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:26:28 +0300

    added repository for Team

--- src/main/java/com/bombaysour/bombaysour/repository/TeamRepository.java
+++ src/main/java/com/bombaysour/bombaysour/repository/TeamRepository.java
@@ -1,4 +1,16 @@
 package com.bombaysour.bombaysour.repository;
 
-public interface TeamRepository {
+import com.bombaysour.bombaysour.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team,Long> {
+
+    List<Team> findAllByAvailable(Boolean available);
+
+    Team findByAvailableAndId(Boolean available, Long id);
+
 }
