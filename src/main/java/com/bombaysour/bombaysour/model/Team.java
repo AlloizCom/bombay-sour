c75fa6f7679f3ab32eb0272ad6f17f2ce87bd2c4
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:04:38 +0300

    added entity for Team

---
+++ src/main/java/com/bombaysour/bombaysour/model/Team.java
@@ -0,0 +1,53 @@
+package com.bombaysour.bombaysour.model;
+
+import javax.persistence.Column;
+
+public class Team extends BaseEntity<Team>{
+
+    private String name;
+    private String image;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String biography;
+
+    public Team() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Team setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Team setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public Team setBiography(String biography) {
+        this.biography = biography;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", biography='" + biography + '\'' +
+                ", id=" + id +
+                ", available=" + available +
+                '}';
+    }
+}
