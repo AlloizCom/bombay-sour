277f249b9281f330bfcc04814bf926c61124f3ec
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:25:19 +0300

    added repository for Story

--- src/main/java/com/bombaysour/bombaysour/repository/StoryRepository.java
+++ src/main/java/com/bombaysour/bombaysour/repository/StoryRepository.java
@@ -1,4 +1,16 @@
 package com.bombaysour.bombaysour.repository;
 
-public interface StoryRepository {
+import com.bombaysour.bombaysour.model.Story;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StoryRepository extends JpaRepository<Story,Long> {
+
+    List<Story> findAllByAvailable(Boolean available);
+
+    Story findByAvailableAndId(Boolean available, Long id);
+
 }
