93b2b5e372e3846130eaaf1a781129aa4b18c0df
unknown <bohdanrud5180@gmail.com>
2018-10-02 17:46:24 +0300

    added DTO for Story

---
+++ src/main/java/com/bombaysour/bombaysour/dto/StoryDto.java
@@ -0,0 +1,61 @@
+package com.bombaysour.bombaysour.dto;
+
+import com.bombaysour.bombaysour.dto.utils.annotations.Dto;
+
+@Dto
+public class StoryDto {
+
+    protected Long id;
+    protected Boolean available;
+    private String name;
+    private String videoUrl;
+
+    public StoryDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public StoryDto setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public StoryDto setAvailable(Boolean available) {
+        this.available = available;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public StoryDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public StoryDto setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "StoryDto{" +
+                "id=" + id +
+                ", available=" + available +
+                ", name='" + name + '\'' +
+                ", videoUrl='" + videoUrl + '\'' +
+                '}';
+    }
+}
