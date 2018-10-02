6566e7f8a6f8f9f072d557fe0ab1f5db0589b426
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:18:59 +0300

    added repository for Film

--- src/main/java/com/bombaysour/bombaysour/repository/FilmRepository.java
+++ src/main/java/com/bombaysour/bombaysour/repository/FilmRepository.java
@@ -1,4 +1,16 @@
 package com.bombaysour.bombaysour.repository;
 
-public interface FilmRepository {
+import com.bombaysour.bombaysour.model.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film,Long> {
+
+    List<Film> findAllByAvailable(Boolean available);
+
+    Film findByAvailableAndId(Boolean available, Long id);
+
 }
