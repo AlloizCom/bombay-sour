9c2c86f292068a1eb1a6435580c1ae5ec47cf0f7
unknown <bohdanrud5180@gmail.com>
2018-10-02 14:20:39 +0300

    added repository for Community

--- src/main/java/com/bombaysour/bombaysour/repository/CommunityRepository.java
+++ src/main/java/com/bombaysour/bombaysour/repository/CommunityRepository.java
@@ -1,4 +1,15 @@
 package com.bombaysour.bombaysour.repository;
 
-public interface CommunityRepository {
+import com.bombaysour.bombaysour.model.Community;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommunityRepository extends JpaRepository<Community,Long> {
+
+    List<Community> findAllByAvailable(Boolean available);
+
+    Community findByAvailableAndId(Boolean available, Long id);
 }
