936feb743d638d4ed7c4abe2f11a55de05e484fc
unknown <bohdanrud5180@gmail.com>
2018-10-03 19:25:00 +0300

    added @EnableCaching

--- src/main/java/com/bombaysour/bombaysour/ServerApplication.java
+++ src/main/java/com/bombaysour/bombaysour/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.bombaysour.bombaysour.repository")
+@EnableCaching
 public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
