5a111ce10300b27ee37ed06197aa0174aa43ecb8
unknown <bohdanrud5180@gmail.com>
2018-10-03 13:50:16 +0300

    configuration for security

---
+++ src/main/java/com/bombaysour/bombaysour/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.bombaysour.bombaysour.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@ComponentScan("com.bombaysour.bombaysour")
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().authorizeRequests()
+//                .formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("name")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/admin", true)
+//                .failureUrl("/")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/").permitAll()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/admin").access("hasRole('ADMIN')")
+//                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+//                .antMatchers("/login").permitAll()
+//                .antMatchers("/").permitAll()
+                .anyRequest().permitAll();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("resources/**");
+    }
+}
