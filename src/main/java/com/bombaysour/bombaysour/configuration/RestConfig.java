c269ae4db4a0dbebdde139dc7a04aa3f502b7ba6
bohdanrud <bohdanrud5180@gmail.com>
2018-10-11 13:17:36 +0300

    fixed filters

--- src/main/java/com/bombaysour/bombaysour/configuration/RestConfig.java
+++ src/main/java/com/bombaysour/bombaysour/configuration/RestConfig.java
@@ -23,7 +23,7 @@ public class RestConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization,Content-Type,Origin,enctype");
@@ -34,7 +34,7 @@ public class RestConfig implements Filter {
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
-            chain.doFilter(req, res);
+            chain.doFilter(req, response);
         }
     }
 
